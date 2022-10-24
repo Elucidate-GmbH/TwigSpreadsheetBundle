cf1d8bc5902d0bdeeaf0dd014d2e649c9dec0d88
Tamás Szigeti <tamas@szigeti.one>
2022-10-24 11:03:08 +0200

    revert

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,7 +17,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('twig_spreadsheet_bundle');
+        $treeBuilder = new TreeBuilder('mewes_k_twig_spreadsheet');
 
         $treeBuilder->getRootNode()
             ->children()
